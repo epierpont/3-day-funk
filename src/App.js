c2af230a31e05c661a747beacc84df75ddc73d8f
Eric Pierpont <epierpont@gmail.com>
2021-11-07 08:24:45 -0500

    Added boostrap and CNAME fix

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import logo from './logo.png';
+import Container from 'react-bootstrap/Container';
 import './App.css';
+import logo from './logo.png';
 
 function App() {
   return (
     <div className="App">
-      <div className="wrapper">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hard Classic Rock and Blues
-          </p>
-
-          <div className="video-container">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/AD82YyvLFI0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-
-          {/*<a
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/*<a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -24,8 +16,17 @@ function App() {
         >
           Learn React
         </a>*/}
-        </header>
-      </div>
+      </header>
+      <Container>
+        <main>
+          <p>
+            Hard Classic Rock and Blues
+          </p>
+          <div className="video-container">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/AD82YyvLFI0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </main>
+      </Container>
     </div>
   );
 }
