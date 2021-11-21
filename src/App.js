293011dd13592671646bf8281909a2abdeea5335
Eric Pierpont <epierpont@gmail.com>
2021-11-21 10:45:37 -0500

    Changed tagline

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
       <Container>
         <main>
           <p>
-            Hard Classic Rock and Blues
+            Hard Classic Rock and Blues!
           </p>
           <div className="video-container">
             <iframe width="560" height="315" src="https://www.youtube.com/embed/AD82YyvLFI0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
